bda00cafb5d93d4a5ff7cbcbfc287ea84f372463
DESKTOP-PDB00V3\avell i7 <walldir@gmail.com>
2018-09-15 20:56:20 -0300

    - Adicionada solucao e projeto Windows Service(Cliente)

---
+++ RemoteController.WindowsService/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RemoteController.WindowsService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
